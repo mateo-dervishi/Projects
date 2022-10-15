1b5fdff3ac46a94bf78de95db7bd6bf48ed29bcf
mateo-dervishi <115823152+mateo-dervishi@users.noreply.github.com>
2022-10-15 17:09:17 +0100

    Update app.js

--- Rock-Paper-Scissors/app.js
+++ Rock-Paper-Scissors/app.js
@@ -5,12 +5,12 @@ const game = () => { //Placing all code within this function ensures there are n
     //Start the Game
     const startGame = () => {
       const playBtn = document.querySelector(".intro button");
-      const introScreen = document.querySelector(".intro");
+      const introScreen = document.querySelector(".intro"); //Here we are setting introScreen to the class: intro allowing us to manipluate it later on
       const match = document.querySelector(".match");
   
       playBtn.addEventListener("click", () => { 
-        introScreen.classList.add("fadeOut"); //This fades out the intro screen
-        match.classList.add("fadeIn"); //This fades in the match screen
+        introScreen.classList.add("fadeOut"); //This fades out the intro screen as we add on a class which makes the opacity 0
+        match.classList.add("fadeIn"); //This fades in the match screen as we add on a class which makes the opacity 1
       });
     };
     //Play Match
@@ -26,7 +26,7 @@ const game = () => { //Placing all code within this function ensures there are n
         option.addEventListener("click", function() {
           //Computer Choice
           const computerNumber = Math.floor(Math.random() * 3); //Generates a random integer, 0-2
-          const computerChoice = computerOptions[computerNumber]; //Choses rock, paper or scissors depending on rgn
+          const computerChoice = computerOptions[computerNumber]; //Choses rock, paper or scissors depending on randomly generated number
   
           //Here is where we call compare hands
           compareHands(this.textContent, computerChoice); //This updates the text after each battle by calling the function and feeding in the 2 values
